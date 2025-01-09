f00ea07f8332ea7415afa104a89ac093b512b42a
Alexandwere <[lazukov.ao@gmail.com]>
2025-01-09 23:55:57 +0300

    chore

--- src/main/java/com/javaacademy/cryptowallet/service/cryptoAccountService/CryptoAccountService.java
+++ src/main/java/com/javaacademy/cryptowallet/service/cryptoAccountService/CryptoAccountService.java
@@ -1,11 +1,17 @@
 package com.javaacademy.cryptowallet.service.cryptoAccountService;
 
 import com.javaacademy.cryptowallet.dto.CryptoAccountDto;
+import com.javaacademy.cryptowallet.dto.OperationMoneyBodyDto;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 public interface CryptoAccountService {
     List<CryptoAccountDto> findAllForUser(String login);
     UUID createCryptoAccount(CryptoAccountDto cryptoAccountDto);
+    void topUpInRub(OperationMoneyBodyDto bodyDto);
+    String withdrawRub(OperationMoneyBodyDto bodyDto);
+    BigDecimal balanceRub(UUID uuid);
+    BigDecimal allBalanceRub(String login);
 }
